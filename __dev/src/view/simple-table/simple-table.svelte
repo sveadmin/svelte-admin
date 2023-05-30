96b881c3fea9909dcea2fda8ad04af8724f56137
Lord_G <marton.kufferstein@gmail.com>
2023-05-30 22:26:29 +0200

    Deciding on TableContextConstructor shape, including moving the editors within actions

--- __dev/src/view/simple-table/simple-table.svelte
+++ __dev/src/view/simple-table/simple-table.svelte
@@ -1,6 +1,49 @@
 <script lang="ts">
-  import Table from '@sveadmin/table'
+  import {
+    createContext,
+    Table,
+  } from '@sveadmin/table'
+  
+  import type {
+    TableContext,
+    TableContextKey,
+  } from '@sveadmin/table'
+
+
+  const contextKey: TableContextKey = {key: 'application-role-list'}
+
+  const context: TableContext = createContext(contextKey, {
+    actions: {
+
+    },
+    data: [
+      {
+        attributes: {
+          id:1,
+          name: 'asdasd'
+        },
+        id:1,
+      },
+      {
+        attributes: {
+          id:3,
+          name: 'hgjghj'
+        },
+        id:3,
+      }
+    ],
+    settings: [
+      {
+        columnVisible: true,
+        field: 'id'
+      },
+      {
+        columnVisible: true,
+        field: 'name'
+      },
+    ]
+  })
+
 </script>
 
-Table without stuff
-<Table />
+<Table {contextKey} />
